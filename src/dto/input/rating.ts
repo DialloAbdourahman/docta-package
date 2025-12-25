44d0fec1a94e1be7b1258c1ee2455b9923debf7d
Diallo Abdourahman <dialliabdourahman78@gmail.com>
2025-12-25 23:22:50 +0100

    small fix

--- src/dto/input/rating.ts
+++ src/dto/input/rating.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsString, Min, Max } from "class-validator";
+import { IsNumber, IsString, Min, Max, IsOptional } from "class-validator";
 
 export class CreateRatingDto {
   @IsNumber()
@@ -7,5 +7,17 @@ export class CreateRatingDto {
   rating!: number;
 
   @IsString()
+  message!: string;
+}
+
+export class UpdateRatingDto {
+  @IsNumber()
+  @Min(1)
+  @Max(5)
+  @IsOptional()
+  rating!: number;
+
+  @IsString()
+  @IsOptional()
   message!: string;
 }
